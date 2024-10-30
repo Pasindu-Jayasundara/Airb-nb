0c3db6d191453d99a953e85fc031e77d8746f66d
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2024-10-30 18:02:14 +0530

    Refactor card component styles and update image source

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,10 @@
 import '../../public/css/card.css';
+import image from '../../public/assets/vite.svg'
 
 export default function Card(){
     return(
         <div className="card">
-            <div className="cardImage"></div>
+            <img src={image} className="cardImage"/>
             <div className="card-text">
                 <h3>Card Title</h3>
                 <p className='card-description'>Card Description Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate assumenda ipsum nam temporibus animi corporis facilis fuga, ipsam a praesentium, aperiam accusantium nisi consequuntur placeat sequi sint quos explicabo vitae?</p>
